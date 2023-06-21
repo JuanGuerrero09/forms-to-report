e630bd1cd22cf9bd845f0b5b73a50a0e3fe46581
Juan David <juanguerrero09mc@gmail.com>
2023-06-21 17:47:22 +0200

    logic changed to include sections

--- form_manipulation.py
+++ form_manipulation.py
@@ -1,37 +1,39 @@
 import json
-from export_excel import generate_report
-  
-# Opening JSON file
-f = open('answers.json')
-g = open('questions.json')
-
-# returns JSON object as 
-# a dictionary
-data = json.load(f)
-questions = json.load(g)
 
 def get_questions():
+    g = open('questions.json')
+    questions = json.load(g)
     items = questions['items']
     all_questions = {}
+    questions_by_section = {}
+    current_section = ''
     for item in items:
+        questionSection = item.get("pageBreakItem")
+        if questionSection != None:
+            current_section = item.get("title")
+            questions_by_section[current_section] = []
         questionItem = item.get('questionItem')
         if questionItem != None:
             question = questionItem.get('question').get('questionId')
+            questions_by_section[current_section].append(question)
             all_questions[question] = item['title']
-    return all_questions
+    questions_by_section['ID'] = '0'
+    all_questions['0']='id'
+    return questions_by_section, all_questions
 
 
 def get_data():
-    question_list = get_questions()
+    f = open('answers.json')
+    data = json.load(f)
+    question_list = get_questions()[1]
     answer_list = data['responses']
     formated_answers = []
 
     for response in answer_list:
         answer_formated = {}
         answer_formated['email'] = response['respondentEmail']
-        time = response["createTime"][0:10]
+        time = response["createTime"][0:16]
         location = ''
-        # print(response["createTime"][0:10], response['answers'][next(iter(response['answers']['textAnswers']))])
         for questionId, answers in response['answers'].items():
             if 'textAnswers' in answers:
                 value = answers['textAnswers']['answers'][0]['value']
@@ -39,24 +41,48 @@ def get_data():
                 if questionId == '6146a411':
                     location = value
                 answer_formated[question] = value
+            if 'fileUploadAnswers' in answers:
+                value = answers['fileUploadAnswers']['answers']
+                question = question_list.get(questionId)
+                img_id_list = []
+                for img in value:
+                    imgId = img['fileId']
+                    img_id_list.append(imgId)
+                answer_formated[question] = img_id_list
         formated_answers.append(answer_formated)
         answer_formated['id'] = f'{time}-{location}'
-    
     return formated_answers
-       
 
 
 def get_ids(info):
-    return [x.get('id') for x in info]
+    return [x.get('ID') for x in info]
 
 def find_element_by_id(arr, id):
     for element in arr:
-        if element.get('id') == id:
+        if element.get('ID') == id:
             return element
     return None
 
-info = get_data()
-selected_report = find_element_by_id(info, '2023-05-31-Tbilisi')
+def get_formated_answers():
+    question_by_section, question_texts = get_questions()
+    data = get_data()
+    all_answers = []
+    # Iterar sobre cada sección en el diccionario original
+    for element in data:
+        # Diccionario final con las secciones, preguntas y respuestas combinadas
+        combined_sections = {}
+        for section, questions in question_by_section.items():
+            # Combinar los elementos correspondientes en un solo diccionario
+            combined = {}
+            for question_id in questions:
+                question_text = question_texts.get(question_id)
+                if question_text:
+                    answer = element.get(question_text)
+                    if answer:
+                        combined[question_text] = answer 
+            combined_sections[section] = combined
+            # Agregar el diccionario combinado a la sección correspondiente
+        combined_sections['ID'] = combined_sections['ID'].get('id')
+        all_answers.append(combined_sections)
+    return all_answers
 
-
-# generate_report(selected_report)
